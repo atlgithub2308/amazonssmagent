63a8420d4b14612279032930377a9faaee3e15fc
atlgithub2308 <89908586+atlgithub2308@users.noreply.github.com>
2021-09-28 11:16:25 +0800

    Create globe.rb

---
+++ lib/facter/globe.rb
@@ -0,0 +1,14 @@
+Facter.add(:globepolicy_1_1) do
+  confine :osfamily => 'Debian'
+  confine :operatingsystemmajrelease => '10'
+  setcode do
+    s = Facter::Core::Execution.exec(
+      '/sbin/sysctl kernel.randomize_va_space'
+    )
+    if ( s != 'kernel.randomize_va_space = 2' )
+      :fail
+    else
+      :pass
+    end
+  end
+end
